9af421756d9bd1a3f1da65f91d787c218cba172d
Richard Go <marchlice@outlook.com>
2026-03-06 12:48:08 +0800

    Avoid preview raster redeclaration in ClusterMapper

---
+++ src/core/greenSubtype.js
@@ -0,0 +1,9 @@
+export const GREEN_SUBTYPES = [
+  { id: 0, name: '未指定' },
+  { id: 1, name: '公园绿地' },
+  { id: 2, name: '防护绿地' },
+  { id: 3, name: '广场用地' },
+  { id: 4, name: '附属绿地' },
+]
+
+export const PARK_GREEN_SUBTYPE_ID = 1
